b0f648034baceb638480a48a76d4852e502cec92
MRKJ1337 <78854077+MRKJ1337@users.noreply.github.com>
2026-09-20 22:41:12 +0200

    Remove CLI argparser

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,6 @@
-use clap::*;
-
 use rasga::prelude::*;
 
-#[derive(Parser)]
-#[command(version, about, long_about = None)]
-struct Args {
-    /// The address to write your shellcode at.
-    #[arg(short, long)]
-    target: u32,
-
-    /// The size of read() to set.
-    #[arg(short, long)]
-    size: u32,
-
-    /// The size of the shellcode to set.
-    #[arg(short, long)]
-    padding_size: u32,
-}
-
 fn main() {
-    // let args = Args::parse();
-
     let mut prng = StdRng::seed_from_u64(0);
     let mut state = State::default();
     state.marker = prng.random_range(1..=9);
@@ -32,9 +12,6 @@ fn main() {
         &mut prng,
         &mut svc_part,
         &mut state,
-        // args.target,
-        // args.size,
-        // args.padding_size,
         0x70776000,
         0x200,
         0x110,
